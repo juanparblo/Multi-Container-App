1a9a4cc223a52dbd5830dcad34144197b88f9d8c
johnpaulnwaz <johnpaulnwaz@outlook.com>
2022-12-03 02:47:48 +0100

    edited the test files

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,4 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // delete the 3 lines inside the body of this thing and make sure it ends ike => {} );
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders learn react link', () => {});
